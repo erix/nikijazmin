fcd69b3475e13d9bf0029901228208c9f7c350b2
Erik Simko <erik.simko@gmail.com>
2013-02-16 11:35:03 +0200

    Project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nikijazmin::Application.config.secret_token = '883e3c60b956f3ed3764fb0eabba804ea92f9434f7dceefd74871f651b52678463167c9b826b50dd746d2ba732c7b0dfa6d8655e755735b65aa38f77c48a473a'
