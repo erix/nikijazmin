703605f583698337ca54652574fd52b7dc45252a
Erik Simko <erik@Eriks-MacBook-Air-3.local>
2013-02-18 19:57:01 +0200

    Blog controller created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Nikijazmin::Application.routes.draw do
   root :to => 'home#index'
   match "/books", :to => 'books#index'
   match "/apps", :to => 'apps#index'
+  match "/blog", :to => 'blog#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
