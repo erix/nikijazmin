105038b6e8c24da77c488c835eb30f27d998264a
Erik Simko <erik@Eriks-MacBook-Air-3.local>
2013-02-18 19:51:15 +0200

    Apps controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Nikijazmin::Application.routes.draw do
   root :to => 'home#index'
   match "/books", :to => 'books#index'
+  match "/apps", :to => 'apps#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
