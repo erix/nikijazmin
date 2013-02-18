9a068f7d85423a2adcf2ae888aeda0945b0307b3
Erik Simko <erik@Eriks-MacBook-Air-3.local>
2013-02-18 19:43:28 +0200

    Indication of selected item in the navigation menu.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_link(name, path)
+    controller_name = name.downcase
+    current = current_page? :controller => controller_name
+    selected = current ? " selected" : ""
+    content_tag "li", link_to(name, path), :class => "#{controller_name}#{selected}"
+  end
 end
