1844f4a85e8f380fb92c71c90eb552c8abb8e08d
Erik Simko <erik@Eriks-MacBook-Air-3.local>
2013-02-20 12:36:38 +0200

    Markdown changed to redcarpet

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ group :production do
     gem 'pg', '0.12.2'
 end
 
-gem 'RedCloth'
+gem 'redcarpet'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     selected = current ? " selected" : ""
     content_tag "li", link_to(name, path), :class => "#{controller_name}#{selected}"
   end
+
+  def dat_markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :no_intra_emphasis => true)
+    markdown.render(text).html_safe
+  end
 end
