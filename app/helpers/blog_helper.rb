7d59ebfbdd19e54d5031b943d5f175bf527793bf
Erik Simko <erik@Eriks-MacBook-Air-3.local>
2013-02-26 14:58:15 +0200

    Facebook share button fixed

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,2 +1,10 @@
 module BlogHelper
+  # enables the languages in facebook like button
+  def fb_source_path
+    locale = {}
+    locale[:hu] = 'hu_HU'
+    locale[:fi] = 'fi_FI'
+    locale[:en] = 'en_US'
+    "//connect.facebook.net/#{locale[I18n.locale]}/all.js#xfbml=1"
+  end
 end
