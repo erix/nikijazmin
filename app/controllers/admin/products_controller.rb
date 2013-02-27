f3f9a047ae367f1cb806304751ddf19a0176dd62
Erik Simko <erik@Eriks-MacBook-Air-3.local>
2013-02-27 17:14:04 +0200

    Admin: product pages

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,4 +3,32 @@ class Admin::ProductsController < Admin::AdminController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.new(params[:product])
+    if product.save
+      flash[:success] = "Product successfully created"
+      redirect_to admin_products_path
+    else
+      flash[:error] = "Failed to create new product"
+      render "new"
+    end
+  end
+
+  def update
+    product = Product.find(params[:id])
+    if product
+      product.update_attributes(params[:product])
+      flash[:success] = "Product successfully updated"
+    end
+    redirect_to admin_products_path
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,27 +1,30 @@
 class Product < ActiveRecord::Base
-  TYPE = {:book => 0, :app => 1}
+  TYPES = {:book => 0, :app => 1}
 
   attr_accessible :banner, :desc, :icon, :mini_banner, :wide_banner, :name, :store_link, :in_slider, :product_type, :release_date, :locale
 
   scope :coming_soon, where(:release_date => nil)
-  scope :books, where(:product_type => TYPE[:book])
-  scope :apps, where(:product_type => TYPE[:app])
+  scope :books, where(:product_type => TYPES[:book])
+  scope :apps, where(:product_type => TYPES[:app])
 
   # use lambda to get around the caching to have current values
   scope :new_products, ->{where(:release_date  => (Time.now.midnight - 1.month)..Time.now.midnight)}
   default_scope order('release_date DESC')
 
   def self.type(t)
-    TYPE[t]
+    TYPES[t]
   end
 
-  def product_type
-    TYPE.key(read_attribute(:product_type))
-  end
+  # def product_type
+  #   TYPES.key(read_attribute(:product_type))
+  # end
 
   def product_type=(t)
+    # accept string, symbol and number
     if t.class == String
       t = t.to_sym
+    elsif t.class == Fixnum
+      t = Product.type(t)
     end
     write_attribute(:product_type, Product.type(t))
   end
