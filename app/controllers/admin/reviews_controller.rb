968bdd56e3a437cfd186d1b17f245239290b2ce9
Erik Simko <erik.simko@gmail.com>
2013-03-06 12:44:17 +0200

    Admin: list and edit reviews

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,15 +1,16 @@
 class Admin::ReviewsController < Admin::AdminController
-  def index
+  before_filter :fetch_product
 
+  def index
+    @reviews = @product.reviews
   end
 
   def new
-    @product = Product.find(params[:product_id])
+    @review = Review.new
   end
 
   def create
-    product = Product.find(params[:product_id])
-    @review = product.reviews.build(params[:review])
+    @review = @product.reviews.build(params[:review])
     if @review.save
       flash[:success] = "New product review created"
       redirect_to products_path
@@ -17,5 +18,25 @@ class Admin::ReviewsController < Admin::AdminController
       flash[:error] = "Failed to create new review"
       redirect_to :new
     end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update_attributes(params[:review])
+      flash[:success] = "Product review updated"
+      redirect_to product_reviews_path(@product)
+    else
+      flash[:error] = "Failed to update review"
+      redirect_to :edit
+    end
+  end
+
+  private
+  def fetch_product
+    @product = Product.find(params[:product_id])
   end
 end
