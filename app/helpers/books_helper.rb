264cb1ea5f699af30e406c46419cd9c37d02cb9b
Erik Simko <erik.simko@gmail.com>
2013-03-10 21:02:12 +0200

    Fix icon_tag

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -4,7 +4,7 @@ module BooksHelper
       :"2x" => book.icon.url,
       :class => "product-icon hisrc"
     }
-    options.merge(:size => book.icon_size(:large)) unless book.icon_size(:large).blank?
+    options.merge!(:size => book.icon_size(:large)) unless book.icon_size(:large).blank?
     responsive_image_tag book.icon.url(:large), options
   end
 end
