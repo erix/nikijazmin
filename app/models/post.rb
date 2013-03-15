5af6e953007ac9cb4ca3a48895760eb00f7a2a85
Erik Simko <erik.simko@gmail.com>
2013-03-15 08:56:30 +0200

    Blog posts are ordered by published date

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :text, :published_at, :locale
 
   scope :published, where("published_at IS NOT NULL")
+  default_scope order("published_at DESC")
 
   def published?
     self.published_at.present?
